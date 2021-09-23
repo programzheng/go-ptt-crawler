3082282b7b97ec73a33268c4fd198e6636c221c3
neil <esz759486@gmail.com>
2021-09-24 00:39:29 +0800

    get previous page link and write json file

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -9,7 +13,38 @@ import (
 	"github.com/gocolly/colly/v2/debug"
 )
 
+const PTT_IMAGE_BOARD_HANDLER_CHUNK_SIZE = 10
+const PTT_IMAGE_BOARD_HANDLER_LIMIT_SIZE = 100
+
+func checkFileExist(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return !os.IsNotExist(err)
+}
+
+func writeJsonFile(fileName string, data []string) {
+	if checkFileExist(fileName) {
+		appendJsonDataByte, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			log.Fatalf("get old images json file data error:\n%v", err)
+		}
+		var appendJsonData []string
+		json.Unmarshal(appendJsonDataByte, &appendJsonData)
+		appendJsonData = append(appendJsonData, data...)
+		data = appendJsonData
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("images to json data error:\n%v", err)
+	}
+	err = ioutil.WriteFile(fileName, jsonData, 0644)
+	if err != nil {
+		log.Fatalf("images json data to json file error:\n%v", err)
+	}
+}
+
 func pttImageBoardHandler(ctx *gin.Context) {
+	var images []string
+
 	board := ctx.Param("board")
 	baseUrl := "https://www.ptt.cc/"
 	url := fmt.Sprintf("/bbs/%v", board)
@@ -18,13 +53,25 @@ func pttImageBoardHandler(ctx *gin.Context) {
 	// Find and visit all article links
 	c.OnHTML("div.r-ent a[href]", func(e *colly.HTMLElement) {
 		//filter 公告
-		title := e.Text
+		text := e.Text
 		link := e.Attr("href")
-		if strings.HasPrefix(title, "[公告]") || !strings.HasPrefix(link, url+"/M.") {
+		if strings.HasPrefix(text, "[公告]") || !strings.HasPrefix(link, url+"/M.") {
+			return
+		}
 			return
 		}
 		articleLink := baseUrl + link
 		e.Request.Visit(articleLink)
+	})
+
+	//Find previous page link
+	c.OnHTML("div.btn-group.btn-group-paging a[href]", func(e *colly.HTMLElement) {
+		text := e.Text
+		link := e.Attr("href")
+		if text == "‹ 上頁" {
+			prevLink := baseUrl + link
+			e.Request.Visit(prevLink)
+		}
 	})
 
 	//Find article content image link
@@ -42,6 +89,16 @@ func pttImageBoardHandler(ctx *gin.Context) {
 		}
 
 		fmt.Println(link)
+		//write json
+		if len(images) > 0 && len(images)%PTT_IMAGE_BOARD_HANDLER_CHUNK_SIZE == 0 {
+			fmt.Println(images)
+			fileName := fmt.Sprintf("ptt_images_%v.json", board)
+			writeJsonFile(fileName, images)
+		} else if len(images) >= PTT_IMAGE_BOARD_HANDLER_LIMIT_SIZE {
+			panic("Exit")
+		} else {
+			images = append(images, link)
+		}
 	})
 
 	c.OnRequest(func(r *colly.Request) {
